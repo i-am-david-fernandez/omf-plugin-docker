5fa5c95cf56a4bb9e3ae0fbecac76f7c8c9e8723
David Fernandez <i.am.david.fernandez@gmail.com>
2017-05-30 17:06:11 +1000

    <DF> Added 'ps' option to override default output formatting.

--- functions/docker.fish
+++ functions/docker.fish
@@ -1,3 +1,19 @@
-function docker -d "My package"
-  # Package entry-point
+function docker -d "Docker command wrapper"
+    # Package entry-point
+    set new_args
+
+    for arg in $argv
+        set -e argv[1]
+
+        switch $arg
+            case 'ps'
+                set new_args $new_args $arg '--format' 'table {{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Status}}\t{{.CreatedAt}}\t{{.Ports}}'
+
+            case '*'
+                set new_args $new_args $arg
+        end
+    end
+
+    command docker $new_args
+
 end
